5c50b3817cce1f45895057f2e321d4cfe65a2dcf
liuxiaomingskm <liuxiaomingskm@gmail.com>
2019-01-31 09:34:22 -0800

    First Commit

---
+++ testgithubdemo/testgithubdemo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  testgithubdemo
+//
+//  Created by 刘晓明 on 1/31/19.
+//  Copyright © 2019 刘晓明. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   
+    std::cout << "Hello, World!\n";
+    return 0;
+}
